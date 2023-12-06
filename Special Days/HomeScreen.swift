575d08b3d2b516e96edfafc14d2450c68aedabd5
sukrusimsek <sukrusimsekll@gmail.com>
2023-12-07 00:21:23 +0300

    first commit

---
+++ Special Days/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Special Days
+//
+//  Created by Şükrü Şimşek on 7.12.2023.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+
+
+}
+
